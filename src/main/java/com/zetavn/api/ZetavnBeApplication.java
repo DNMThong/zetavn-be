0b6d9252dc98432994ba8d770121a3a287c03dd9
dnmthong <duongthong07@gmail.com>
2023-09-27 19:09:41 +0700

    Initial commit

---
+++ src/main/java/com/zetavn/api/ZetavnBeApplication.java
@@ -0,0 +1,13 @@
+package com.zetavn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZetavnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZetavnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zetavn/api/ZetavnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetavn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZetavnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
