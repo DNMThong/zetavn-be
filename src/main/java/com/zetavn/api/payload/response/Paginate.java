3184e5e46ca072dcbf13b0fd115243d74d2d581b
nguyenhoangquan2003 <nguyenhoangquan2003vn@gmail.com>
2023-10-14 18:10:04 +0700

    update try catch api post-like

---
+++ src/main/java/com/zetavn/api/payload/response/Paginate.java
@@ -0,0 +1,16 @@
+package com.zetavn.api.payload.response;
+
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder @ToString
+public class Paginate<T> {
+
+    private T data;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean lastPage;
+}
