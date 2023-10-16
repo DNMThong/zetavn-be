0c9ed988f235fc09738aa7b37bec07cf105e25db
dnmthong <duongthong07@gmail.com>
2023-10-16 18:18:23 +0700

    fix

--- src/main/java/com/zetavn/api/controller/UserController.java
+++ src/main/java/com/zetavn/api/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.zetavn.api.controller;
 
 import com.zetavn.api.payload.response.ApiResponse;
+import com.zetavn.api.service.PostService;
 import com.zetavn.api.service.UserService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v0/users")
@@ -16,6 +14,9 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    PostService postService;
 
     @GetMapping("")
     public ApiResponse<?> getAllUsers() {
@@ -26,5 +27,10 @@ public class UserController {
                                             @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
                                             @RequestParam(value = "pageSize", defaultValue = "5", required = false) Integer pageSize) {
         return userService.getAllUsersByKeyword(kw, pageNumber, pageSize);
+    }
+
+    @GetMapping("/{userId}/posts")
+    public ApiResponse<?> getAllPostByUserId(@PathVariable String userId) {
+        return postService.getAllPostByUserId(userId);
     }
 }
