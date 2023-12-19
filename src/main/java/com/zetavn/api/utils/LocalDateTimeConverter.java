37890f9bf3add682749549055f74fbdb815527c3
dnmthong <duongthong07@gmail.com>
2023-12-19 17:00:55 +0700

    update

--- src/main/java/com/zetavn/api/utils/LocalDateTimeConverter.java
+++ src/main/java/com/zetavn/api/utils/LocalDateTimeConverter.java
@@ -1,29 +1,29 @@
-package com.zetavn.api.utils;
-
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
-
-import java.sql.Timestamp;
-import java.time.*;
-
-@Converter(autoApply = true)
-public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
-
-    @Override
-    public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
-        OffsetDateTime timeUtc = attribute.atOffset(ZoneOffset.UTC);
-        OffsetDateTime offsetTime = timeUtc.withOffsetSameInstant(ZoneOffset.ofHours(7));
-        System.out.println(offsetTime.toLocalDateTime().toString());
-        return (attribute == null ? null : Timestamp.valueOf(offsetTime.toLocalDateTime()));
-    }
-
-    @Override
-    public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
-        ZonedDateTime utcZonedDateTime = ZonedDateTime.of(dbData.toLocalDateTime(), ZoneId.of("UTC"));
-        ZonedDateTime vietnamZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.systemDefault());
-        LocalDateTime vietnamDateTime = vietnamZonedDateTime.toLocalDateTime();
-
-        return (dbData == null ? null : vietnamDateTime);
-    }
-}
-
+//package com.zetavn.api.utils;
+//
+//import jakarta.persistence.AttributeConverter;
+//import jakarta.persistence.Converter;
+//
+//import java.sql.Timestamp;
+//import java.time.*;
+//
+//@Converter(autoApply = true)
+//public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+//
+//    @Override
+//    public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+////        OffsetDateTime timeUtc = attribute.atOffset(ZoneOffset.UTC);
+////        OffsetDateTime offsetTime = timeUtc.withOffsetSameInstant(ZoneOffset.ofHours(7));
+////        System.out.println(offsetTime.toLocalDateTime().toString());
+//        return (attribute == null ? null : Timestamp.valueOf(attribute));
+//    }
+//
+//    @Override
+//    public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+//        ZonedDateTime utcZonedDateTime = ZonedDateTime.of(dbData.toLocalDateTime(), ZoneId.of("UTC"));
+//        ZonedDateTime vietnamZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.systemDefault());
+//        LocalDateTime vietnamDateTime = vietnamZonedDateTime.toLocalDateTime();
+//
+//        return (dbData == null ? null : vietnamDateTime);
+//    }
+//}
+//
